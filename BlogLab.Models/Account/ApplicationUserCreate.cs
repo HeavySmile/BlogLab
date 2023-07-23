b69023daac6f2ea22e7cc505b64a9ffec6d5287b
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-23 20:31:19 +0300

    Add class library for data models, implement user account data models

---
+++ BlogLab.Models/Account/ApplicationUserCreate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserCreate : ApplicationUserLogin
+    {
+        private const string _fullnameLengthError = "Must be at least 10-30 characters";
+        private const string _emailLengthError = "Can be at most 30 characters";
+        private const string _emailRequiredError = "Email is required";
+        private const string _emailAddressValidationError = "Invalid email format";
+
+        [MinLength(10, ErrorMessage = _fullnameLengthError)]
+        [MaxLength(30, ErrorMessage = _fullnameLengthError)]
+        public string Fullname { get; set; }
+
+        [Required(ErrorMessage = _emailRequiredError)]
+        [MaxLength(30, ErrorMessage = _emailLengthError)]
+        [EmailAddress(ErrorMessage = _emailAddressValidationError)]
+        public string Email { get; set; }
+    }
+}
---
+++ BlogLab.Models/Account/ApplicationUserLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Account
+{
+    public class ApplicationUserLogin
+    {
+        private const string _requiredError = "Username is required";
+        private const string _usernameLengthError = "Must be at least 5-20 characters";
+        private const string _passwordLengthError = "Must be at least 10-50 characters";
+
+        [Required(ErrorMessage = _requiredError)]
+        [MinLength(5, ErrorMessage = _usernameLengthError)]
+        [MaxLength(20, ErrorMessage = _usernameLengthError)]
+        public string Username { get; set; }
+        
+        [Required(ErrorMessage = _requiredError)]
+        [MinLength(10, ErrorMessage = _passwordLengthError)]
+        [MaxLength(50, ErrorMessage = _passwordLengthError)]
+        public string Password { get; set; }
+    }
+}
