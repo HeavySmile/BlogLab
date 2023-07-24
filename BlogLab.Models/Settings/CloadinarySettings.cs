eec6bf6128fcf1a1a7da6a999285b32ca6bd5a6f
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-24 11:00:03 +0300

    Add settings data model for Cloudinary web service

---
+++ BlogLab.Models/Settings/CloadinarySettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogLab.Models.Settings
+{
+    public class CloadinarySettings
+    {
+        public string CloadName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}
