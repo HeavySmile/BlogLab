2206307a275750b89e26ca07d35c370a1643ddb3
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-27 12:50:11 +0300

    Fix typo in file/class name

--- BlogLab.Models/Settings/CloudinarySettings.cs
+++ BlogLab.Models/Settings/CloudinarySettings.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BlogLab.Models.Settings
 {
-    public class CloadinarySettings
+    public class CloudinarySettings
     {
         public string CloadName { get; set; }
         public string ApiKey { get; set; }
