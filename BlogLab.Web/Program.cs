b4ea14679a6539ea373f3dbe9eb438053736ca6f
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-27 13:10:25 +0300

    Temporary settings configuration

--- BlogLab.Web/Program.cs
+++ BlogLab.Web/Program.cs
@@ -1,7 +1,12 @@
+using BlogLab.Models.Settings;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+var cloudinarySettings = builder.Configuration.GetSection("CloudinarySettings");
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.Configure<CloudinarySettings>(cloudinarySettings);
 
 var app = builder.Build();
 
