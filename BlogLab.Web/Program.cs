adbc56fdb1dbdcedafe9c0e85aa3b206674f3f6a
Leonid Shubin <lenyashubin2002@gmail.com>
2023-07-28 11:24:32 +0300

    Add app services and authentication

--- BlogLab.Web/Program.cs
+++ BlogLab.Web/Program.cs
@@ -1,12 +1,63 @@
+using BlogLab.Identity;
+using BlogLab.Models.Account;
 using BlogLab.Models.Settings;
+using BlogLab.Repository;
+using BlogLab.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 var cloudinarySettings = builder.Configuration.GetSection("CloudinarySettings");
+
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.Configure<CloudinarySettings>(cloudinarySettings);
+
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IPhotoService, PhotoService>();
+
+builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+builder.Services.AddScoped<IBlogCommentRepository, BlogCommentRepository>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+builder.Services.AddIdentityCore<ApplicationUserIdentity>(stgs => { stgs.Password.RequireNonAlphanumeric = false; })
+    .AddUserStore<UserStore>()
+    .AddDefaultTokenProviders()
+    .AddSignInManager<SignInManager<ApplicationUserIdentity>>();
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+
+builder.Services.AddAuthentication(stgs => 
+{ 
+    stgs.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+    stgs.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+    stgs.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(stgs => 
+    {
+        stgs.RequireHttpsMetadata = false;
+        stgs.SaveToken = true;
+        stgs.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = cloudinarySettings["Jwt:Issuer"],
+            ValidAudience = cloudinarySettings["Jwt:Issuer"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(cloudinarySettings["Jwt:Key"])),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
 
 var app = builder.Build();
 
