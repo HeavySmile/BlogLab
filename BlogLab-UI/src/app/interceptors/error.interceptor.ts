c4792b402494f85c4153d4ae36bca953fa9596c7
HeavySmile <lenyashubin2002@gmail.com>
2023-09-07 12:47:49 +0300

    Finish error handling service

--- BlogLab-UI/src/app/interceptors/error.interceptor.ts
+++ BlogLab-UI/src/app/interceptors/error.interceptor.ts
@@ -23,16 +23,16 @@ export class ErrorInterceptor implements HttpInterceptor {
         if (error) {
           switch (error.status) {
             case 400: 
-              // handle 400 error
+              this.handle400Error(error);
               break;
             case 401: 
-              // handle 401 error
+              this.handle401Error(error);
               break;
             case 500: 
-              // handle 500 error
+              this.handle500Error(error);
               break;
             default:
-              // handle Unexpected
+              this.handleUnexpectedError(error);
               break;
           }
         }
@@ -40,5 +40,54 @@ export class ErrorInterceptor implements HttpInterceptor {
         return throwError(() => error);
       })
     );
+  }
+
+  handle400Error(error: any) {
+    if (!!error.error && Array.isArray(error.error)) {
+      let errorMessage = '';
+      for (const key in error.error) {
+        const errorElement = error.error[key];
+        if (!!errorElement) {
+          errorMessage = (`${errorMessage}${errorElement.code} - ${errorElement.description}\n`)
+        }
+      }
+      this.toastr.error(errorMessage, error.statusText);
+      console.log(error.error);
+    } else if (!!error?.error.errors?.Content && (typeof error.error.errors.Content) === 'object') {
+      let errorObject = error.error.errors.Content;
+      let errorMessage = '';
+      for (const key in errorObject) {
+        const errorElement = errorObject[key];
+        errorMessage = (`${errorMessage}${errorElement}\n`)
+      }
+      this.toastr.error(errorMessage, error.status);
+      console.log(error.error);
+    } else if (!!error.error) {
+      let errorMessage = ((typeof error.error) == 'string')
+        ? error.error
+        : 'There was a validation error';
+      this.toastr.error(errorMessage, error.statusCode);
+      console.log(error.error);
+    } else {
+      this.toastr.error(error.statusText, error.status);
+      console.log(error);
+    }
+  } 
+
+  handle401Error(error: any) {
+    let errorMessage = 'Please login to your account';
+    this.accountService.logout();
+    this.toastr.error(errorMessage, error.statusText);
+    // route to the login form
+  }
+
+  handle500Error(error: any) {
+    this.toastr.error('Please contact the administrator. An error happened on the server');
+    console.log(error);
+  }
+
+  handleUnexpectedError(error: any) {
+    this.toastr.error('Something unexpected happened');
+    console.log(error);
   }
 }
