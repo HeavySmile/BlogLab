c95cdaef41739214667e379b82643ba7b3772157
HeavySmile <lenyashubin2002@gmail.com>
2023-09-07 20:43:47 +0300

    Implement authentication guard

--- BlogLab-UI/src/app/guards/auth.guard.ts
+++ BlogLab-UI/src/app/guards/auth.guard.ts
@@ -1,5 +1,28 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { AccountService } from '../services/account.service';
+//
+// !!!!!! Check for validity
+//
+class PermissionsService {
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  constructor(
+    private accountService: AccountService,
+    private router: Router
+  ) {}
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const currentUser = this.accountService.currentUserValue;
+    const isLoggedIn = currentUser && currentUser.token;
+
+    if (isLoggedIn) return true;
+
+    this.router.navigate(['/']);
+
+    return false;
+  }
+}
+
+export const AuthGuard: CanActivateFn = (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean => {
+  return inject(PermissionsService).canActivate(route, state);
 };
